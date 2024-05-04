fba3a3ef8a9ead8015d01a299fe3b5f4a275180e
A Jakhar <125469531+jakharamit44@users.noreply.github.com>
2024-05-04 14:56:02 +0530

    Update Config.ts

--- api/src/Config.ts
+++ api/src/Config.ts
@@ -2,10 +2,10 @@
 const DB_API_URL = "https://stunning-determination-production.up.railway.app";
 
 // Variables below must be same as used by bot
-const UPLOADER_BOTS_1 = ["bot_token_1", "bot_token_2"]
-const UPLOADER_BOTS_2 = ["bot_token_3", "bot_token_4"]
+const UPLOADER_BOTS_1 = ["7092415941:AAE6fvm2DAANcHgCFuIFQbrb2JUqPaRtqog", "7005857065:AAEwa-AaD-OLIdjLziCN-mvmMosPCLaIBKA"]
+const UPLOADER_BOTS_2 = ["7167715976:AAE7D6nkm2ELCgAsXK7IGUOlpkGFxdad4Vo", "6751989885:AAEIxK_wQSmbk3UgeT3Y6zxGmCgeUF3HNMA"]
 
-const STORAGE_CHANNEL_1 = -1001234567890
-const STORAGE_CHANNEL_2 = -1001234567890
+const STORAGE_CHANNEL_1 = -1002091415759
+const STORAGE_CHANNEL_2 = -1002091415759
 
 export { DB_API_URL, UPLOADER_BOTS_1, UPLOADER_BOTS_2, STORAGE_CHANNEL_1, STORAGE_CHANNEL_2};
